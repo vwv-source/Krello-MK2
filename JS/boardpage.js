1e9ca28064661406614803033f498cd82d748b79
vwv-source <abdullahmohfathy@gmail.com>
2022-09-26 00:58:27 +0200

    addedsomeshit!

--- JS/boardpage.js
+++ JS/boardpage.js
@@ -1,24 +1,13 @@
-var cards = document.querySelectorAll('.card');
-var lists = document.querySelectorAll('.list');
+//-----------------------Variables and imports---------------
+
 var draggedcard = null;
+var draggedlist = null;
 var selectedlist = null;
+var dragmode = null;
 var bincontainer = document.querySelector('.bincontainer')
 import 'https://code.jquery.com/jquery-3.6.0.min.js'
 
-$(document).on('click', '.addcard', function(){
-    $(document.body).append('<div class="popup"><div class="window"><input type="text" class="textinput" value="Card title."><br><input type="button" class="button" value="Create" id="createcardbutt"><input type="button" class="button" value="Cancel" id="cancelbutt"></div></div>');
-    selectedlist = $(this).parent();
-    $(document).on('keyup', function(e) {
-        if(e.key == "Escape"){
-            $('.popup').remove()
-        }
-    })
-})
 
-$(document).on('click', '#createcardbutt', function(e){
-    e.preventDefault()
-    $(selectedlist).find('.cardscontainer').append(`<div class="card" draggable="true"><p class="cardtext">${$(this).parent().find(".textinput").val()}</p></div>`);
-})
 
 //--------------Bin stuff---------------------
 
@@ -45,9 +34,9 @@ bincontainer.addEventListener("drop", function binDrop(e){
 
 //--------------Bin stuff---------------------
 
-$(document).on('dragstart', '.card', function(e){
-    draggedcard = $(this);
-})
+//--------------Window/popup stuffs-------------
+
+
 
 $(document).on('click','#cancelbutt', function(e){
     $('.popup').remove()
@@ -64,12 +53,79 @@ $(document).on('click', '.card', function(e){
     });
 })
 
+$(document).on('click', '.addcard', function(){
+    $(document.body).append('<div class="popup"><div class="window"><input type="text" class="textinput" value="Card title."><br><input type="button" class="button" value="Create" id="createcardbutt"><input type="button" class="button" value="Cancel" id="cancelbutt"></div></div>');
+    selectedlist = $(this).parent();
+    $(document).on('keyup', function(e) {
+        if(e.key == "Escape"){
+            $('.popup').remove()
+        }
+    })
+})
+
+$(document).on('click', '#createcardbutt', function(e){
+    e.preventDefault()
+    $(selectedlist).find('.cardscontainer').append(`<div class="card" draggable="true"><p class="cardtext">${$(this).parent().find(".textinput").val()}</p></div>`);
+})
+
+//--------------Window/popup stuffs-------------
+
+//--------------List Dragging--------------------
+
+$(document).on('dragstart', '.list', function(e){
+    if(dragmode == null){
+        dragmode = 0;
+        draggedlist = $(this);
+    }
+})
+
+$(document).on('dragstart', '.card', function(e){ 
+    if(dragmode == null){
+        dragmode = 1;
+        draggedcard = $(this);
+    }
+})
+
 $(document).on('dragover', '.list', function(e){
-    $(this).find('.cardscontainer').append(draggedcard)
+    e.preventDefault();  
+    if(dragmode == 0){
+        if($(this).index() == 1){
+            $(this).before(draggedlist);
+        }else{
+            $(this).after(draggedlist);
+        }
+    }else if(dragmode == 1){
+        $(this).find('.cardscontainer').append(draggedcard)
+    }
 })
 
 $(document).on('drop', '.list', function(e){
-    $(this).find('.cardscontainer').append(draggedcard)
+    e.preventDefault();
+    if(dragmode == 0){
+        if($(this).index() == 1){
+            $(this).before(draggedlist);
+        }else{
+            $(this).after(draggedlist);
+        }
+        dragmode = null;
+        draggedlist = null;
+    }
+    else if(dragmode == 1){
+        $(this).find('.cardscontainer').append(draggedcard)
+        dragmode = null;
+        draggedcard = null;
+    }
+    
 })
 
+//--------------List Dragging--------------------
+
+/* $(document).on('dragover', '.list', function(e){
+    $(this).find('.cardscontainer').append(draggedcard)
+}) */
+
+/* $(document).on('drop', '.list', function(e){
+    
+})
+ */
 
